4c3838742f92ef14e89b8a900c3ef2586c832ae4
Pedro Henrique Pereira <pedrohp36@gmail.com>
2023-02-02 11:26:00 -0300

    .Criação da classe "SairException"; .Criação da classe "TabuleiroException"; .Modificação e novos metodos nas classes "Aplicacao", "Campo" e "Tabuleiro".

--- src/main/java/br/com/pedrohp28/cm/modelo/Tabuleiro.java
+++ src/main/java/br/com/pedrohp28/cm/modelo/Tabuleiro.java
@@ -1,5 +1,7 @@
 package br.com.pedrohp28.cm.modelo;
 
+import br.com.pedrohp28.cm.excecao.ExplosaoException;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Predicate;
@@ -23,10 +25,15 @@ public class Tabuleiro {
     }
 
     public void abrir(int linha, int coluna) {
-        campos.stream()
-                .filter(c -> c.getLinha() == linha && c.getColuna() == coluna)
-                .findFirst()
-                .ifPresent(c -> c.abrir());
+        try {
+            campos.stream()
+                    .filter(c -> c.getLinha() == linha && c.getColuna() == coluna)
+                    .findFirst()
+                    .ifPresent(c -> c.abrir());
+        } catch (ExplosaoException e) {
+            campos.forEach(c -> c.setAberto(true));
+            throw e;
+        }
     }
 
     public void alternarMarcacao(int linha, int coluna) {
@@ -57,9 +64,9 @@ public class Tabuleiro {
         Predicate<Campo> minado = c -> c.isMinado();
 
         do {
-            minasArmadas = campos.stream().filter(minado).count();
             int aleatorio = (int)(Math.random() * campos.size());
             campos.get(aleatorio).minar();
+            minasArmadas = campos.stream().filter(minado).count();
         } while (minasArmadas < minas);
     }
 
@@ -76,8 +83,19 @@ public class Tabuleiro {
     public String toString() {
         StringBuilder sb = new StringBuilder();
 
+        sb.append("  ");
+        for (int c = 0; c < colunas; c++) {
+            sb.append(" ");
+            sb.append(c);
+            sb.append(" ");
+        }
+
+        sb.append("\n");
+
         int k = 0;
         for (int i = 0; i < linhas; i++) {
+            sb.append(i);
+            sb.append(" ");
             for (int j = 0; j < colunas; j++) {
                 sb.append(" ");
                 sb.append(campos.get(k));
